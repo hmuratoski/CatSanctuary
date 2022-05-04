b296254c5862e382c60ef55147663e16fc4102aa
hmuratoski <98337481+hmuratoski@users.noreply.github.com>
2022-05-04 22:01:16 +0300

    JS DOM manupulation added to header;

--- JS/advancedjavacat.js
+++ JS/advancedjavacat.js
@@ -5,3 +5,17 @@ let h2=document.getElementById("cardadvanced1");
 h.classList.toggle("shading");
 h2.classList.toggle("shading");
 
+
+
+
+let header=document.querySelector("h2");
+header.addEventListener("mouseover" ,ChangeOptions)
+
+function ChangeOptions(){
+header.classList.toggle("important");
+}
+
+
+
+
+
